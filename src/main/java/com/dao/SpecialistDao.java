7608bede0c29fa3090eed918371e7e955c128d48
pradosmitapani <panipradosmita00@gmail.com>
2024-10-03 16:08:37 +0530

    SpecialistDao

--- src/main/java/com/dao/SpecialistDao.java
+++ src/main/java/com/dao/SpecialistDao.java
@@ -1,8 +1,13 @@
 package com.dao;
 
+import com.entity.Specalist;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SpecialistDao {
     private Connection conn;
@@ -27,5 +32,31 @@ public class SpecialistDao {
         }
 
         return f;
+    }
+    public List<Specalist> getAllSpesialist()
+    {
+        List<Specalist> list=new ArrayList<Specalist>();
+        Specalist s=null;
+
+        try {
+
+            String sql="select * from specialist";
+            PreparedStatement ps=conn.prepareStatement(sql);
+
+            ResultSet rs= ps.executeQuery();
+            while (rs.next()){
+                s=new Specalist();
+                s.setId(rs.getInt(1));
+                s.setSpecialistName(rs.getString(2));
+                list.add(s);
+
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+
+        }
+
+        return list;
     }
 }
