6be5b25105a5b02b8341ae37fdcdb859aa7cfd85
madhusmita <madhusmitsahoo63222@gmail.com>
2024-10-03 23:14:42 +0530

    update something

--- src/main/java/com/dao/SpecialistDao.java
+++ src/main/java/com/dao/SpecialistDao.java
@@ -2,7 +2,9 @@ package com.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 
 import com.entity.Specialist;
@@ -33,5 +35,27 @@ public class SpecialistDao {
             e.printStackTrace();
         }
         return f;
+    }
+    public List<Specialist> getSpecialist()
+    {
+        List<Specialist> list = new ArrayList<Specialist>();
+        Specialist s=null;
+
+        try {
+            String sql = "select * from specialist";
+            PreparedStatement ps = conn.prepareStatement(sql);
+
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()) {
+                s = new Specialist();
+                s.setId(rs.getInt(1));
+                s.setSpecalistName(rs.getString(2));
+                list.add(s);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
     }
 }
