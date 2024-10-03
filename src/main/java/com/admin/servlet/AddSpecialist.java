ac702e5962e84ceaaef4f2d9b0581b889f3d8114
Rajashree556 <routrajashree84@gmail.com>
2024-10-03 18:15:31 +0530

    add

--- src/main/java/com/admin/servlet/AddSpecialist.java
+++ src/main/java/com/admin/servlet/AddSpecialist.java
@@ -1,7 +1,6 @@
 package com.admin.servlet;
 
-import com.dao.SpecialistDao;
-import com.db.DBConnect;
+import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,26 +8,36 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
 
+import com.dao.SpecialistDao;
+import com.db.DBConnect;
+import com.entity.User;
 @WebServlet("/addSpecialist")
-public class AddSpecialist extends HttpServlet {
+public class AddSpecialist extends HttpServlet{
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String specName=req.getParameter("specName");
-
-        SpecialistDao dao=new SpecialistDao(DBConnect.getConn());
+        SpecialistDao dao= new SpecialistDao(DBConnect.getConn());
         boolean f=dao.addSpecialist(specName);
 
-        HttpSession session= req.getSession();
+        HttpSession session=req.getSession();
 
-        if (f){
-            session.setAttribute("succMsg","Specialist Added");
+
+
+        if(f)
+        {
+
+            session.setAttribute("succMsg", "Specialist Added");
             resp.sendRedirect("admin/index.jsp");
+
         }else {
-            session.setAttribute("errorMsg","something wrong on server");
-            resp.sendRedirect("admin/index.jsp");
 
+            session.setAttribute("errorMsg", "Something wrong on server");
+            resp.sendRedirect("admin_login.jsp");
         }
+
     }
+
+
 }
