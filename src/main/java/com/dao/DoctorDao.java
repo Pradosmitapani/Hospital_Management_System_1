e53d8c67111f576abaaf4ff490f2de9e2feaf3f7
Rajashree556 <routrajashree84@gmail.com>
2024-10-03 19:23:47 +0530

    doctor

--- src/main/java/com/dao/DoctorDao.java
+++ src/main/java/com/dao/DoctorDao.java
@@ -1,4 +1,43 @@
 package com.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.entity.Doctor;
+
 public class DoctorDao {
+
+    private Connection conn;
+
+
+    public DoctorDao(Connection conn) {
+        super();
+        this.conn = conn;
+    }
+
+    public boolean registerDoctor(Doctor d) {
+        boolean f = false;
+        try {
+            String sql = "insert into doctor(full_name,dob,qualification,specialist,email,mobno,password) values(?,?,?,?,?,?,? )";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, d.getFullName());
+            ps.setString(2, d.getDob());
+            ps.setString(3, d.getQualification());
+            ps.setString(4, d.getSpecialist());
+            ps.setString(5, d.getEmail());
+            ps.setString(6, d.getMobNo());
+            ps.setString(7, d.getPassword());
+            int i = ps.executeUpdate();
+            if (i == 1) {
+                f = true;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return f;
+    }
 }
