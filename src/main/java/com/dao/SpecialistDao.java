4e62229c735725759bb5bcafc6f9c4f92f03ef9d
Rajashree556 <routrajashree84@gmail.com>
2024-10-03 18:11:35 +0530

    changes

--- src/main/java/com/dao/SpecialistDao.java
+++ src/main/java/com/dao/SpecialistDao.java
@@ -15,3 +15,25 @@ public class SpecialistDao {
         super();
         this.conn = conn;
     }
+
+    public boolean addSpecialist(String spec) {
+
+        boolean f = false;
+        try {
+            String sql = "insert into specialist(spec_name) values(?)";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, spec);
+            int i = ps.executeUpdate();
+
+            if (i == 1) {
+                f = true;
+            }
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+
+        return f;
+    }
+}
