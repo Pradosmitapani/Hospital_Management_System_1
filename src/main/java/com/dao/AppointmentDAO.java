16fae15912432a1de3bdff6e0999761206a396ef
pradosmitapani <panipradosmita00@gmail.com>
2024-10-19 08:23:58 +0530

    Appointment user

--- src/main/java/com/dao/AppointmentDAO.java
+++ src/main/java/com/dao/AppointmentDAO.java
@@ -1,4 +1,56 @@
 package com.dao;
 
+import com.entity.Appointment;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class AppointmentDAO {
+
+    private Connection conn;
+
+    public AppointmentDAO(Connection conn) {
+        super();
+        this.conn = conn;
+    }
+
+    public boolean addAppointment(Appointment ap){
+        boolean f=false;
+
+        try {
+
+            String sql="insert into appointment(user_id,fullname,gender,age,appoint_date,email,phno,diseases,doctor_id,address,status) values(?,?,?,?,?,?,?,?,?,?,?)";
+            PreparedStatement ps= conn.prepareStatement(sql);
+            ps.setInt(1, ap.getUserId());
+            ps.setString(2, ap.getFullName());
+            ps.setString(3, ap.getGender());
+            ps.setString(4, ap.getAge());
+            ps.setString(5, ap.getAppoinDate());
+            ps.setString(6, ap.getEmail());
+            ps.setString(7, ap.getPhNo());
+            ps.setString(8, ap.getDiseases());
+            ps.setString(9, ap.getDoctorId());
+            ps.setString(10, ap.getAddress());
+            ps.setString(11, ap.getStatus());
+
+
+
+            int i= ps.executeUpdate();
+            if (i==1){
+                f = true;
+            }
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+        return f;
+    }
 }
