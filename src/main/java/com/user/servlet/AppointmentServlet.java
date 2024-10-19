06708507a316bf90dbaf8f5e5308b016e41c8935
pradosmitapani <panipradosmita00@gmail.com>
2024-10-19 08:34:30 +0530

    appointservlet

---
+++ src/main/java/com/user/servlet/AppointmentServlet.java
@@ -0,0 +1,25 @@
+package com.user.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AppointmentServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+
+        int userId = Integer.parseInt(req.getParameter("userId"));
+        String fullName = req.getParameter("fullName");
+        String gender = req.getParameter("gender");
+        String age = req.getParameter("age");
+        String appoint_date = req.getParameter("appoint_date");
+        String email = req.getParameter("email");
+        String phno = req.getParameter("phno");
+        String diseases=req.getParameter("diseases");
+        int doctor_id =Integer.parseInt(req.getParameter("doct"));
+        String address = req.getParameter("address");
+    }
+}
