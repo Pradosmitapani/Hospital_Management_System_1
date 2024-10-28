ca0e796f4ee5f6026af48f1b3d91a538a4e88fa7
Rajashree556 <routrajashree84@gmail.com>
2024-10-28 15:43:58 +0530

    some changes

--- src/main/java/com/dao/UserDao.java
+++ src/main/java/com/dao/UserDao.java
@@ -1,65 +1,110 @@
 package com.dao;
 
-import com.entity.User;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import com.entity.User;
+
 public class UserDao {
+
     private Connection conn;
 
-//    Generate constructor
     public UserDao(Connection conn) {
+        super();
         this.conn = conn;
     }
 
-    public boolean register (User u){
-       boolean f = false; //Something wrong on server
+    public boolean register(User u) {
+        boolean f = false;
 
-        try{
-            String sql = "insert into user_dtls(full_name,email,password) values(?,?,?)";
+        try {
+            String sql = "insert into user_dtls(full_name,email,password) values(?,?,?) ";
 
-            // Set the value in database
             PreparedStatement ps = conn.prepareStatement(sql);
             ps.setString(1, u.getFullName());
             ps.setString(2, u.getEmail());
             ps.setString(3, u.getPassword());
 
-            int i=ps.executeUpdate();
+            int i = ps.executeUpdate();
 
-            // If data successfully inserted then return true
-            if(i==1){
+            if (i == 1) {
                 f = true;
             }
 
-        }catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        return f; // If data successfully not inserted then return false
+
+        return f;
     }
 
-    public User login (String em, String psw){
+    public User login(String em, String psw) {
         User u = null;
 
-        try{
+        try {
             String sql = "select * from user_dtls where email=? and password=?";
-            PreparedStatement ps=conn.prepareStatement(sql);
+            PreparedStatement ps = conn.prepareStatement(sql);
             ps.setString(1, em);
             ps.setString(2, psw);
 
-            ResultSet rs=ps.executeQuery();
+            ResultSet rs = ps.executeQuery();
 
-            while(rs.next()){
+            while (rs.next()) {
                 u = new User();
                 u.setId(rs.getInt(1));
                 u.setFullName(rs.getString(2));
                 u.setEmail(rs.getString(3));
                 u.setPassword(rs.getString(4));
             }
-        }catch (Exception e) {
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
+
         return u;
     }
+
+    public boolean checkOldPassword(int userid, String oldPassword) {
+        boolean f = false;
+
+        try {
+            String sql = "select * from user_dtls where id=? and password=?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, userid);
+            ps.setString(2, oldPassword);
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                f = true;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return f;
+    }
+
+    public boolean changePassword(int userid, String newPassword) {
+        boolean f = false;
+
+        try {
+            String sql = "update user_dtls set password=? where id=?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, newPassword);
+            ps.setInt(2, userid);
+
+            int i = ps.executeUpdate();
+            if (i == 1) {
+                f = true;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return f;
+    }
+
 }
--- src/main/java/com/util/DBConnect.java
+++
@@ -1,9 +0,0 @@
-package com.util;
-
-import java.sql.Connection;
-
-public class DBConnect {
-    public static Connection getConn() {
-        return null;
-    }
-}
